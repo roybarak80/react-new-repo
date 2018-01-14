e517cea9434a7c6d5e227cdb4b503fce7b483cc1
Roy Barak <roy.barak@tcwd.fr>
2018-01-14 14:23:21 +0200

    Routes Lesson

---
+++ src/components/posts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Roy Barak on 14-Jan-18.
+ */
+import React,{ Component } from 'react';
+
+class Posts extends Component{
+    render(){
+        return <div>Posts</div>
+    }
+}
+
+export default Posts;
---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Roy Barak on 14-Jan-18.
+ */
+import React,{ Component } from 'react';
+
+class Profile extends Component{
+    render(){
+        return <div>Profile</div>
+    }
+}
+
+export default Profile;
